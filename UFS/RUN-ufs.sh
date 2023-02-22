10fea33281491f6bd135c713c99b894a206e011c
NeilBarton-NOAA <neil.barton@noaa.gov>
2023-02-22 21:43:44 +0000

    merge between machines

--- UFS/RUN-ufs.sh
+++ UFS/RUN-ufs.sh
@@ -11,7 +11,7 @@ export TEST_NAME=S2S
 export ATM_RES="C384"
 export OCN_RES="025"
 export FORECAST_LENGTH=16 #in days
-export WALLCLOCK=5 # in hours
+export WALLCLOCK=6 # in hours
 export UFS_EXEC=ufs_S2SWA_mixed_mode
 REPO=NeilBarton-NOAA
 branch=run
@@ -22,8 +22,8 @@ export ATM_INPES=16
 export ATM_JNPES=12
 export ATM_THRD=1
 export CHM_NMPI=0
-export OCN_NMPI=220
-export ICE_NMPI=120
+export OCN_NMPI=160
+export ICE_NMPI=80
 export WAV_NMPI=0
 #MED (defaults to ATM)
 export MED_NMPI=300
@@ -31,7 +31,7 @@ export MED_THRD=${ATM_THRD}
 
 ############
 # IO options
-export ATM_WPG=60
+export ATM_WPG=96
 #export MOM6_IO_LAYOUT='4,2'
 export RESTART_FREQ=$(( FORECAST_LENGTH * 24 ))
 
