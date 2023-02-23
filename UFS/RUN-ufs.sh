82e3cefd909409b0dd8442862639725fcba73953
NeilBarton-NOAA <neil.barton@noaa.gov>
2023-02-23 14:03:01 +0000

    edits to run script

--- UFS/RUN-ufs.sh
+++ UFS/RUN-ufs.sh
@@ -19,21 +19,24 @@ export DEBUG=F
 ####################################
 # Set MPI options,  if NMPI=0, model will not run
 export ATM_INPES=16
-export ATM_JNPES=12
+export ATM_JNPES=16
 export ATM_THRD=1
 export CHM_NMPI=0
 export OCN_NMPI=220
-export ICE_NMPI=120
+export ICE_NMPI=320
 export WAV_NMPI=0
 #MED (defaults to ATM)
 export MED_NMPI=300
 export MED_THRD=${ATM_THRD}
 
 ############
-# IO options
-export ATM_WPG=60
-#export MOM6_IO_LAYOUT='4,2'
-export RESTART_FREQ=$(( FORECAST_LENGTH * 24 ))
+# IO options                          defaults
+#export ATM_WPG=60                  # 48
+#export MOM6_IO_LAYOUT='4,2'        # 1,1
+#export RESTART_FREQ=3              # forecast length
+#export OUTPUT_FREQ=3               # forecast length
+#export WRITE_DPOST=.true.          # .false.
+#export CICE_OUTPUT=T               # F
 
 ############
 # Submit Forecast
@@ -42,9 +45,9 @@ export UFS_HOME=${NPB_WORKDIR}/CODE/ufs-weather-model_${branch}_${REPO}
 ${UFS_HOME}/RUN/UFS-submit.sh ${NPB_WORKDIR}/RUNs/UFS/\
 ${TEST_NAME}_\
 ATM_${ATM_INPES}${ATM_JNPES}-${ATM_THRD}_\
-ATMIO_${ATM_WPG}_\
 OCN_${OCN_NMPI}_\
-ICE_${ICE_NMPI}_\
-RESTART_N_${RESTART_FREQ} #_\
+ICE_${ICE_NMPI}
+#ATMIO_${ATM_WPG}_\
+#RESTART_N_${RESTART_FREQ} #_\
 #$( date +%s )
 
