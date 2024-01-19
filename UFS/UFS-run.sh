4e1a0161c8bea338c9f5df1c49b581ef66bab3a6
NeilBarton-NOAA <neil.barton@noaa.gov>
2024-01-19 19:56:31 +0000

    defaults ufs-run

--- UFS/UFS-run.sh
+++ UFS/UFS-run.sh
@@ -6,42 +6,17 @@ set -u
 # C96 (~100 km), C192 (~50 km), C384 (25 km), C768 (~13 km), C1152 (~9km)
 ####################################
 # Set Top options
-#export DTG=2017100503
-#export DTG=2013040200
 export DTG=2013040100
-export ICDIR=/scratch2/NCEPDEV/stmp3/Neil.Barton/RUNS/RTs/rt_304936/cpld_bmark_p8_intel
-#export ICDIR=${NPB_WORKDIR}/ICs/${DTG} 
-export CICE_OUTPUT=T
 export ENS_SETTINGS=T
-#export CICE_RESTART='.false.'
-#export ice_ic='default'
-export TOP_RUNDIR=MED_TESTING
-#DIFF=0.05
-#ICE=${DIFF}
-export TEST_NAME=RT
-#EDGE_${DIFF}_ENS3
-#export ice_ic=${ICDIR}/iced.2017-10-05-10800.nc
-#export ice_ic=${ICDIR}/newIC_${DIFF}_iced.2017-10-05-10800.nc
-#export ice_ic=${ICDIR}/newIC_EDGE_Thickness${DIFF}_iced.2017-10-05-10800.nc
-#export ice_ic=${ICDIR}/newIC_EDGE_${DIFF}_iced.2017-10-05-10800.nc
-#export ice_ic=${ICDIR}/newIC_THICKNESSDIFF_${DIFF}_ICE_0.05_iced.2017-10-05-10800.nc
-#export ice_ic=${ICDIR}/testIC.nc
-#export ice_ic=${ICDIR}/philCICEic2.nc
-#export ice_ic=${ICDIR}/newIC_DIFFlimit_0.15_old_iced.2017-10-05-10800.nc
-#export TEST_NAME=DEV_TEST
-#export TEST_NAME=REPLAY_TEST_S2SWA_EP4_ORIG_LIMIT015_ENS3_${DTG}
-#export TEST_NAME=TEST 
 export FORECAST_LENGTH=16 # in days
 export WALLCLOCK=360
 #export JOB_QUEUE=debug # batch or debug on hera
 export UFS_EXEC=ufs_S2SWA 
 export DEBUG=F
 #REPO=ufs-community && HASH=develop
-#REPO=NeilBarton-NOAA && HASH=run
-REPO=rmontuoro && HASH=gefs/ep4_newCICE
-#REPO=rmontuoro && HASH=gefs/ep4/newCICE
+REPO=NeilBarton-NOAA && HASH=run
 PATH_RUN=${NPB_WORKDIR}/CODE/ufs-weather-model_run_NeilBarton-NOAA/RUN 
-RUNDIR_MPI=F
+RUNDIR_MPI=T
 
 ####################################
 # Resolution Options
@@ -52,27 +27,25 @@ RUNDIR_MPI=F
 # Model Options
 export GOCART_NO3=F
 export WAV_RES=glo_025
-#export GRID_SPEC_FILE=${PWD}/grid_spec.nc
-#export FV3_FIX_DIR=/scratch2/NCEPDEV/stmp1/Sanath.Kumar/my_grids
 
 ####################################
 # Set MPI options,  if NMPI=0, model will not run
 export ATM_INPES=8
 export ATM_JNPES=8
-export ATM_THRD=2
-#export CHM_NMPI=$(( ATM_INPES * ATM_JNPES * 6 ))
-export OCN_NMPI=130
+export ATM_THRD=1
+export CHM_NMPI=$(( ATM_INPES * ATM_JNPES * 6 ))
+export OCN_NMPI=150
 export ICE_NMPI=72
-export WAV_NMPI=0 #262
+export WAV_NMPI=262
 #export WAV_THRD=1
 #export MED_NMPI=300
 
 ############
 # IO options                        # DEFAULTS
-#export ATM_WPG=6                   # 48
-#export MOM6_IO_LAYOUT='3,2'        # 1,1
-#export RESTART_FREQ=1             # restart writeout (hours, all components)
-#export OUTPUT_FREQ=3               # forecast length (FV3 and MOM6)
+export ATM_WPG=48                   # 48
+export MOM6_IO_LAYOUT='1,4'         # 1,1
+export RESTART_FREQ=48              # restart writeout (hours, all components)
+export OUTPUT_FREQ=3                # forecast length (FV3 and MOM6)
 #export CICE_OUTPUT=T               # F
 
 ############
