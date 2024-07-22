d20dd5ae6bf1159a24b432e4226a329c1a0b2292
NeilBarton-NOAA <neil.barton@noaa.gov>
2024-07-22 15:28:13 -0500

    updates for RUNTESTS

--- UFS/GW-run.sh
+++ UFS/GW-run.sh
@@ -5,10 +5,10 @@ set -u
 # CI yamls can be found at ${HOMEgfs}/ci/cases/{pr/weekly}/
 ####################################
 # Code
-REPO=NeilBarton-NOAA && HASH=EP5r2
+REPO=NeilBarton-NOAA && HASH=gefs_replay_ci
 HOMEgfs=${1:-${NPB_WORKDIR}/CODE/global-workflow_${HASH////\_}_${REPO}}
-YAML=${2:-${HOME}/UFS/YAMLS-GW/EP5r2.yaml}
-export START_DATE=2021072900
+YAML=${2:-${HOMEgfs}/ci/cases/pr/C96_S2S_gefs_replay_ics.yaml}
+#export START_DATE=2021072900
 
 ########################
 # Check Code
@@ -17,16 +17,10 @@ export START_DATE=2021072900
 echo "HOMEgfs: ${HOMEgfs}"
 echo "YAML: ${YAML}"
 
-export pslot=$(basename ${YAML/.yaml*})_${START_DATE}
-if [[ ${HOMEgfs} == *develop* ]]; then
-    pslot=dev_${pslot}
-fi
+export pslot=$(basename ${YAML/.yaml*})
 CD=$(dirname "$0")
 source ${CD}/MACHINE-config.sh
-if [[ ${YAML} == *CI* ]]; then
-    source ${HOMEgfs}/ci/platforms/config.${m/.*}
-    source ${CD}/MACHINE-config.sh
-fi
+source ${HOMEgfs}/ci/platforms/config.${m/.*}
 source ${HOMEgfs}/workflow/gw_setup.sh
 echo $HPC_ACCOUNT
 export HPC_ACCOUNT=${ACCNR}
@@ -34,11 +28,12 @@ ${HOMEgfs}/workflow/create_experiment.py --yaml "${YAML}"
 
 ################################################
 # Soft link items into EXPDIR for easier development
-cd ${TOPEXPDIR}/${pslot}
+TOPEXPDIR=${RUNTESTS}/EXPDIR/${pslot}
 set +u
-source ${TOPEXPDIR}/${pslot}/config.base
+source ${TOPEXPDIR}/config.base
 set -u
-ln -s ${RUNDIR} RUNDIR
+cd ${TOPEXPDIR}
+ln -s ${DATAROOT} DATAROOT
 ln -s ${HOMEgfs} GW-CODE
 ln -s ${HOMEgfs}/parm/config ORIG_CONFIGS
 ln -s ${COMROOT}/${PSLOT}/logs LOGS_COMROOT
@@ -50,6 +45,6 @@ ln -s ${HOMEgfs}/workflow/rocoto_viewer.py .
 xml_file=${PWD}/${pslot}.xml && db_file=${PWD}/${pslot}.db && cron_file=${PWD}/${pslot}.crontab
 rocotorun -d ${db_file} -w ${xml_file}
 crontab -l | cat - ${cron_file} | crontab -
-# echo crontab file
+echo crontab file
 echo "db=${db_file}"
 echo "xml=${xml_file}"
--- UFS/MACHINE-config.sh
+++ UFS/MACHINE-config.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
 machine=$(uname -n)
 # g-w options
-export TOPEXPDIR=${NPB_WORKDIR}/RUNS
-export TOPCOMROOT=${NPB_WORKDIR}/RUNS/COMROOT
 export TOPICDIR=${NPB_WORKDIR}/ICs
+export RUNTESTS=${NPB_WORKDIR}/RUNS
+
 export ACCNR=marine-cpu
 if [[ ${machine:0:3} == hfe ]]; then
     export module_file=ufs_hera.intel
-    m=hera.intel
 elif [[ ${machine} == *[cd]login* ]]; then
     module_file=ufs_wcoss2.intel
-    m=wcoss2.intel
     # changes from defaults above
     export ACCNR=GFS-DEV
     export TOPCOMROOT=/lfs/h2/emc/ptmp/neil.barton
 elif [[ ${machine} == *Orion* ]]; then
     module_file=ufs_orion.intel
-    m=orion
     export HOMEDIR=/work/noaa/global/'${USER}'
 elif [[ ${machine} == hercules* ]]; then
     module_file=ufs_hercules.intel
-    m=hercules
     export HOMEDIR=/work/noaa/global/'${USER}'
 else
     echo "in ~/UFS/MACHINE-config.sh"
     echo "machine unknown: " $machine
     exit 1
 fi
+export m=${module_file:4:10}
 export HPC_ACCOUNT=${ACCNR}
+
