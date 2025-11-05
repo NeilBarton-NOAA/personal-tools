736ded3eb905edd11a2a17e7a18d092b42d979a2
NeilBarton-NOAA <neil.barton@noaa.gov>
2025-11-05 13:50:52 -0500

    quick map changes

--- ANALYSIS/quick_map.py
+++ ANALYSIS/quick_map.py
@@ -31,57 +31,37 @@ var = args.var[0]
 save_dir = os.environ.get('NPB_WORKDIR') + '/FIGURES'
 os.makedirs(save_dir, exist_ok=True)
 
+def get_valid_names(ds, name_list = ['lat', 'lats', 'TLAT', 'geolat']):
+    """
+    Finds and returns the first variable from name_list that
+    exists in the dataset.
+    """
+    for name in name_list:
+        if name in ds:
+            lat_name = name
+            lon_name = 'TLON' if lat_name == 'TLAT' else name.replace('lat', 'lon')
+            return lat_name, lon_name # Return the DataArray immediatel
+
 for i, f in enumerate(files):
     print(f, var)
-    try: # xarray
-        data = xr.open_dataset(f)
-        if 'nlats' in data.dims:
-            lat_name, lon_name = 'lats', 'lons'
-        elif 'nj' in data.dims:
-            lat_name, lon_name = 'TLAT', 'TLON'
-        elif 'yh' in data.dims:
-            lat_name, lon_name = 'geolat', 'geolon'
-        elif 'lats' in data.dims:
-            lat_name, lon_name = 'lats', 'lons'
-        else:
-            lat_name, lon_name = 'lat', 'lon'
-        #fgeo='/scratch1/NCEPDEV/da/Henry.Winterbottom/scratch/Rahul.Mahajan/cdo_out.nc'
-        #datgeo = xr.open_dataset(fgeo)
-        #lat = datgeo['lat']
-        #lon = datgeo['lon']
-        print(data.dims)
-        print('lat/lon names', lat_name, lon_name)
-        lat = data[lat_name]
-        lon = data[lon_name]
-        plot_data = data[var]
-        if 'time' in plot_data.dims:
-            print('AVE over time')
-            plot_data = plot_data.mean('time')
-        if 'member' in plot_data.dims:
-            print('AVE over member')
-            plot_data = plot_data.mean('member')
-        if 'tau' in plot_data.dims:
-            print('AVE over tau')
-            plot_data = plot_data.mean('tau')
-        if 'z_l' in plot_data.dims:
-            print('First Level')
-            plot_data = plot_data.isel(z_l = 0)
-        if 'hi' in var or 'aice' in var:
-            plot_data = plot_data.where( plot_data > 0 )
-
-        # mask MOM6 NaN values
-        # mask CICE NaN values
-        plot_data = plot_data.where(plot_data < 1e+20)
-    except: #netcdf4
-        print('xarray did not work')
-        file_id = nc.Dataset(f,'r')
-        plot_data = file_id.variables[var][:]
-        if plot_data.shape[0] == 1:
-            plot_data = np.squeeze(plot_data)
-        lat = file_id.variables['lats'][:]
-        lon = file_id.variables['lons'][:]
-        print(lon)
-        file_id.close()
+    ds = xr.open_dataset(f)
+    lat_name, lon_name = get_valid_names(ds)
+    lat, lon = ds[lat_name], ds[lon_name]
+    plot_data = ds[var].squeeze()
+    if len(plot_data.shape) > 2:
+        print('Taking Means over non lat/lon dimensions:', plot_data.dims)
+        dims_to_save = plot_data.dims[:-2]
+        plot_data = plot_data.mean(dim = dims_to_save)
+    if 'tmask' in ds.variables:
+        plot_data = plot_data.where(ds['tmask'] == 1)
+        #print('in here')
+    #exit(1)
+    if var in ['hi', 'aice']:
+        plot_data = plot_data.where(plot_data > 1)
+    #print(ds)
+    #print(plot_data)
+    #exit(1)
+    plot_data = plot_data.where(plot_data < 1e+20)
     # use the same colorbar for all figures
     if i == 0:
         try:
@@ -90,14 +70,11 @@ for i, f in enumerate(files):
         except:
             v_min = np.min(plot_data)
             v_max = np.max(plot_data)
-    if (plot_data.shape != lat.shape):
-        lon, lat = np.meshgrid(lon,lat)
+    #if (plot_data.shape != lat.shape):
+    #    lon, lat = np.meshgrid(lon,lat)
     #if lon.shape == (721, 1440):
     #    lon, lat, plot_data = npb.maptools.index_lon_lat_dat(lon.values, lat.values, plot_data.values)
-    try:
-        print(plot_data.min().values, plot_data.max().values)
-    except:
-        print(np.min(plot_data), np.max(plot_data))
+    print(plot_data.min().values, plot_data.max().values)
     for domain in ['Arctic', 'Antarctic', 'Global']:
     #for domain in ['Antarctic']:
         fig = plt.figure()
