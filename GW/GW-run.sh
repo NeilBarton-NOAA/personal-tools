d56e9ea8e624a36ab873a8413f2786cba1a401dc
NeilBarton-NOAA <neil.barton@noaa.gov>
2025-11-25 15:43:24 -0500

    changes to how run GW

--- GW/GW-run.sh
+++ GW/GW-run.sh
@@ -1,102 +1,33 @@
 #!/bin/sh
 set -u
+source ${PWD}/functions.sh && machine_config
 ####################################
-# set up GW runs with YAML file
-# CI yamls can be found at ${HOMEgfs}/ci/cases/{pr/weekly}/
+# set up GW runs 
 # https://global-workflow.readthedocs.io/en/latest/
 ####################################
-source ${PWD}/REPO_YAML
-BUILD=F
-CI_GFS=F && CI_GEFS=F && CI_SFS=T && CI_GCAPS=F
-UPDATE_MODULES=F
-SFS_BASELINE=F
-ONLY_CHECKOUT=F
+# Code
+#REPO=NeilBarton-NOAA && HASH=ocnice_products
+#REPO=YangxingZheng-NOAA && HASH=sfs_products
+#REPO=XiaqiongZhou-NOAA && HASH=SFSbeta0.1
+REPO=NOAA-EMC && HASH=dev/sfs
+#REPO=NOAA-EMC && HASH=develop
 
-HOMEgfs=${1:-${NPB_WORKDIR}/CODE/gw_${HASH////\_}_${REPO}}
+CI_FORECASTS=F && CI_DA=F 
+SFS_BASELINE=F
+CLONE_ONLY=F
 
 ####################################
-# Clone Model if code doesn't exists
-SCRIPT_DIR=$(readlink -f "$0") && SCRIPT_DIR=$(dirname ${SCRIPT_DIR})
-if [[ ! -d ${HOMEgfs} ]]; then
-    TOPDIR=${HOMEgfs}/../
-    mkdir -p ${TOPDIR} && cd ${TOPDIR}
-    git clone --recursive -b ${HASH} git@github.com:${REPO}/global-workflow.git $(basename ${HOMEgfs})
-    [[ $? > 0 ]] && exit 1
-fi
-[[ ${ONLY_CHECKOUT} == T ]] && exit 0
-
-################################################
-# Check if Code exist
-echo "HOMEgfs: ${HOMEgfs}"
-[[ ! -d ${HOMEgfs} ]] && echo "code is not at ${HOMEgfs}" &&  exit 1
-VARS=(${CI_GFS} ${CI_GEFS} ${CI_SFS} ${CI_GCAPS}) && CI_TESTING=F
-for v in "${VARS[@]}"; do if [[ ${v} == "T" ]]; then CI_TESTING=T; break; fi; done
-if [[ "${CI_TESTING}" == "F" ]]; then
-    YAML_DIR=${YAML_DIR:-$(dirname ${YAML})}
-    YAML_NAME=${YAML_NAME:-$(basename ${YAML%.yaml})}
-    echo "YAML: ${YAML_DIR}"
-    [[ ! -d ${YAML_DIR} ]] && echo "yaml directory does not exist at ${YAML_DIR}" &&  exit 1
-else
-    echo "YAML: Performing CI tests"
-fi
-
-################################################
-# Machine Specific and Personalized options
-export RUNTESTS=${NPB_WORKDIR}/RUNS
-TOPICDIR=${NPB_WORKDIR}/ICs
-machine=$(uname -n)
-case ${machine} in
-    *Orion*)     m=orion ;    
-                 RUNDIRS=/work/noaa/stmp/${USER}/ORION/RUNDIRS ;
-                 TOPICDIR=/work/noaa/marine/Yangxing.Zheng/ICs ;;
-    hercules*)   m=hercules ; 
-                 RUNDIRS=/work2/noaa/stmp/${USER}/HERCULES/RUNDIRS ; 
-                 TOPICDIR=/work/noaa/marine/Yangxing.Zheng/ICs ;;
-    gaea*)       m=gaeac6 ;   
-                 RUNDIRS=/gpfs/f6/${COMPUTE_ACCOUNT}/world-shared/${USER}/RUNDIRS ;
-                 TOPICDIR=/gpfs/f6/sfs-emc/proj-shared/Yangxing.Zheng/SFS/ICs ;;
-    u*)          m=ursa ;     
-                 RUNDIRS=/scratch4/NCEPDEV/stmp/${USER}/RUNDIRS ; 
-                 TOPICDIR=/scratch4/NCEPDEV/global/Yangxing.Zheng/ICs ;;
-    *[cd]login*) m=wcoss2 ;;
-esac
-export TOPICDIR=${TOPICDIR}
-
-################################################
-# Run Generate Workflow
-OPTIONS=()
-[[ ${BUILD} == T ]] && OPTIONS+=("-b")
-[[ ${UPDATE_MODULES} == T ]] && OPTIONS+=("-u")
-if [[ "${CI_TESTING}" == "F" ]]; then
-    OPTIONS+=('-Y') && OPTIONS+=("${YAML_DIR}")
-    OPTIONS+=('-y') && OPTIONS+=("${YAML_NAME}")
-else
-    [[ ${CI_GFS:-T} == T ]] && OPTIONS+=("-G")
-    [[ ${CI_GEFS:-T} == T ]] && OPTIONS+=("-E")
-    [[ ${CI_SFS:-T} == T ]] && OPTIONS+=("-S")
-    [[ ${CI_GCAPS:-T} == T ]] && OPTIONS+=("-C")
-fi
-OPTIONS+=("-D")
-OPTIONS+=("-c")
-cd ${HOMEgfs}/dev/workflow
-echo "RUNNING: ./generate_workflows.sh ${OPTIONS[@]}"
-./generate_workflows.sh "${OPTIONS[@]}" ${RUNTESTS}
-cd ${SCRIPT_DIR}
-
-################################################
-# Soft link items into EXPDIR for easier development
-ln -sf ${RUNTESTS}/EXPDIR .
-if [[ "${CI_TESTING}" == "F" ]]; then
-    for YAML in ${YAML_NAME}; do
-        TOPEXPDIR=${RUNTESTS}/EXPDIR/${YAML}
-        COMROOT=${RUNTESTS}/COMROOT
-        [[ ${SFS_BASELINE} == T ]] && ${SCRIPT_DIR}/SFS-add_basline_dates.sh ${TOPEXPDIR}/${YAML}.xml
-        ln -sf ${HOMEgfs} ${TOPEXPDIR}/GW-CODE
-        ln -sf ${HOMEgfs}/dev/workflow/rocoto_viewer.py ${TOPEXPDIR}
-        ln -sf ${HOMEgfs}/dev/parm/config ${TOPEXPDIR}/ORIG_CONFIGS
-        ln -sf ${COMROOT}/${YAML}/logs ${TOPEXPDIR}/LOGS_COMROOT
-        ln -sf ${RUNDIRS}/${YAML} ${TOPEXPDIR}/RUNDIRS
-    done
-    echo "FINISHED: soft-linking to EXPDIR"
-fi
+# The work to set up an experiment
+HOMEgfs=${NPB_WORKDIR}/CODE/gw_${HASH////\_}_${REPO}
+clone_gw ${HOMEgfs} ${HASH} ${REPO}
+[[ ${CLONE_ONLY:-F} == T ]] && echo "Only Cloning g-w" && exit 0
+get_yamls T ${CI_FORECASTS} ${CI_DA} 
+build_gw ${HOMEgfs} ${COMPUTE_ACCOUNT} ${YAML[@]}
+create_experiment ${HOMEgfs} ${m} ${COMPUTE_ACCOUNT} ${YAML[@]}
+link_EXPDIR ${HOMEgfs} ${YAML[@]}
+[[ ${SFS_BASELINE:-F} == T ]] && sfs_baseline ${YAML[@]}
+add_to_crontab ${m} ${YAML[@]}
+source ~/.profile
+echo " "
 rocotoMONITOR
+
---
+++ GW/functions.sh
@@ -0,0 +1,169 @@
+#!/bin/sh
+set -u
+################################################
+# Machine Specific and Personalized options
+machine_config() {
+export RUNTESTS=${NPB_WORKDIR}/RUNS #/gw_${HASH////\_}_${REPO}
+TOPICDIR=${NPB_WORKDIR}/ICs
+machine=$(uname -n)
+case ${machine} in
+*Orion*)     m=orion ;    
+             TOPICDIR=/work/noaa/marine/Yangxing.Zheng/ICs ;;
+hercules*)   m=hercules ; 
+             TOPICDIR=/work/noaa/marine/Yangxing.Zheng/ICs ;;
+gaea*)       m=gaeac6 ;   
+             TOPICDIR=/gpfs/f6/sfs-emc/proj-shared/Yangxing.Zheng/SFS/ICs ;;
+             #TOPICDIR=${NPB_WORKDIR}/ICs ;; 
+u*)          m=ursa ;     
+             TOPICDIR=/scratch4/NCEPDEV/global/Yangxing.Zheng/ICs ;;
+*[cd]login*) m=wcoss2 ;;
+esac
+export TOPICDIR=${TOPICDIR}
+}
+
+################################################
+# Machine Specific and Personalized options
+get_yamls() {
+DEFAULT_YAMLS=${1}
+CI_FORECAST=${2:-F}
+CI_DA=${3:-F}
+if [[ ${DEFAULT_YAMLS} == T ]]; then
+    YAML=()
+    for f in $( find ${PWD}/YAMLS -name C*.yaml ); do YAML+=("${f}"); done
+fi
+if [[ ${CI_FORECAST} == T ]]; then
+    for f in $( find ${HOMEgfs}/dev/ci/cases/pr -name *.yaml | xargs grep -l forecast-only | xargs grep -L ${machine} ); do YAML+=("$f"); done
+fi 
+if [[ ${CI_DA} == T ]]; then
+    for f in $( find ${HOMEgfs}/dev/ci/cases/pr -name *.yaml | xargs grep -l cycled | xargs grep -L ${machine} ); do YAML+=("$f"); done
+fi 
+export YAML
+}
+
+################################################
+# Clone gw if needed
+clone_gw () {
+HOMEgfs=${1}
+HASH=${2}
+REPO=${3}
+if [[ ! -d ${HOMEgfs} ]]; then
+    ORDIR=${PWD}
+    TOPDIR=${HOMEgfs}/../
+    mkdir -p ${TOPDIR} && cd ${TOPDIR}
+    git clone --recursive -b ${HASH} git@github.com:${REPO}/global-workflow.git $(basename ${HOMEgfs})
+    [[ $? > 0 ]] && exit 1
+    cd ${ORDIR}
+fi
+}
+
+################################################
+# Build only gw options that are needed 
+build_gw () {
+HOMEgfs=${1}
+ACCOUNT=${2}
+YAMLS=${@:3}
+# See of the link_workflow.sh script needs to run
+cd ${HOMEgfs}/sorc
+[[ ! -L ${HOMEgfs}/fix/mom6 ]] && sh link_workflow.sh
+# Check what options need to be compiled
+OPTIONS=()
+for f in ${YAMLS}; do
+    net=$( grep net ${f} | awk '{print $2}' )
+    found=F
+    for OPTION in ${OPTIONS[@]}; do
+        [[ ${OPTION} == ${net} ]] && found=T && break 
+    done
+    [[ "${found}" == "F" ]] && [[ ! -f ${HOMEgfs}/exec/${net}_model.x ]] && OPTIONS+=("${net}")
+done 
+if [[ ${#OPTIONS[@]} -gt 0 ]]; then
+    echo "Building: ${OPTIONS[@]}"
+    sh build_compute.sh -A ${ACCOUNT} ${OPTIONS[@]} >& ~/GW/build_$(basename ${HOMEgfs}).log &
+fi
+}
+
+################################################
+# Run create_experiment script 
+create_experiment () {
+HOMEgfs=${1}
+machine=${2}
+export HPC_ACCOUNT=${3}
+YAMLS=${@:4}
+local YAML
+source ${HOMEgfs}/dev/ci/platforms/config.${machine/.*}
+source ${HOMEgfs}/dev/ush/gw_setup.sh >& /dev/null
+export HPC_ACCOUNT=${COMPUTE_ACCOUNT}
+for YAML in ${YAMLS}; do
+    export pslot=$(basename ${YAML/.yaml*})_$(basename ${HOMEgfs})
+    ${HOMEgfs}/dev/workflow/create_experiment.py --yaml "${YAML}"
+    eval "$(PDY=0 cyc=0 source "${RUNTESTS}/EXPDIR/${pslot}/config.base" >& /dev/null; echo DATAROOT="${STMP}/RUNDIRS/${PSLOT}")"
+    [[ -d ${DATAROOT} ]] && echo "Removing ${DATAROOT}" && rm -r ${DATAROOT}
+done
+}
+
+################################################
+# link items to EXPDIR for personal debugging 
+link_EXPDIR () {
+HOMEgfs=${1}
+YAMLS=${@:2}
+local YAML
+ln -sf ${RUNTESTS}/EXPDIR ${PWD}/GW 
+for YAML in ${YAMLS}; do
+    pslot=$(basename ${YAML/.yaml*})_$(basename ${HOMEgfs})
+    EXPDIR=${RUNTESTS}/EXPDIR/${pslot}
+    COMROOT=${RUNTESTS}/COMROOT/${pslot}
+    eval "$(PDY=0 cyc=0 source "${RUNTESTS}/EXPDIR/${pslot}/config.base" >& /dev/null; echo DATAROOT="${STMP}/RUNDIRS/${PSLOT}")"
+    ln -sf ${HOMEgfs} ${EXPDIR}/GW-CODE 
+    ln -sf ${HOMEgfs}/dev/workflow/rocoto_viewer.py ${EXPDIR}/rocoto_viewer.py
+    ln -sf ${HOMEgfs}/dev/parm/config ${EXPDIR}/ORIG_CONFIGS
+    ln -sf ${COMROOT}/logs ${EXPDIR}/LOGS_COMROOT
+    ln -sf ${DATAROOT} ${EXPDIR}/DATAROOT
+done
+echo "FINISHED: soft-linking to EXPDIR"
+}
+
+################################################
+# add dates to yamls to run for SFS baseline 
+sfs_baseline () {
+YAMLS=${@:1}
+local YAML
+for YAML in ${YAMLS}; do
+    pslot=$(basename ${YAML/.yaml*})_$(basename ${HOMEgfs})
+    EXPDIR=${RUNTESTS}/EXPDIR/${pslot}
+    XML_FILE=${EXPDIR}/${pslot}.xml
+    line=$(grep -n 'cycledef group' ${XML_FILE} | cut -d: -f1) 
+    sed -i ${line}d ${XML_FILE}
+    MONTHS="05 11"
+    for Y in $(seq 1994 2023); do
+        for M in ${MONTHS}; do 
+            text="<cycledef group='"gefs"'>${Y}${M}010000 ${Y}${M}010000 24:00:00</cycledef>"
+            sed -i "${line} i   ${text}" ${XML_FILE}
+            line=$(( line + 1))
+        done
+    done
+    echo "Added all SFS dates to ${XML_FILE}"
+done
+}
+
+################################################
+# add to cronttab if not currenting in crontab 
+add_to_crontab () {
+machine=${1}
+YAMLS=${@:2}
+if [[ ${machine} == gaea* ]] || [[ ${machine} == GAEA* ]]; then
+    ct=scrontab
+else
+    ct=crontab
+fi
+for YAML in ${YAMLS}; do
+    pslot=$(basename ${YAML/.yaml*})_$(basename ${HOMEgfs})
+    f=${RUNTESTS}/EXPDIR/${pslot}/${pslot}
+    exist=$( ${ct} -l | grep ${f} 2>/dev/null | wc -l )
+    if (( ${exist} > 0 )); then
+        echo "Already in Crontab ${pslot}"
+    else
+        echo "Adding to crontab: ${YAML}.crontab"
+        scrontab -l | cat - ${f}.crontab | scrontab -
+    fi
+done
+}
+
